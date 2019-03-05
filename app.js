70c28b1ed16bb9f77c99758f8f9e2f662b37754d
Shayan Anoushiravani <shayananoushiravani@calvisitor-10-105-131-111.calvisitor.1918.berkeley.edu>
2019-03-05 10:52:26 -0800

    added js and consolelogs

--- app.js
+++ app.js
@@ -17,11 +17,28 @@ $(document).submit(function () {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-         
+         var apiresponse = response.response.docs;
+
         console.log(response);
+console.log(apiresponse);
+        for (i = 0; i < apiresponse.length; i++) {
 
         
+        var headline = apiresponse[i].headline.main;
+        var author = apiresponse[i].byline.original;
+        var date = apiresponse[i].pub_date;
+        var url = apiresponse[i].web_url;
 
+        console.log(headline);
+        console.log(author);
+        console.log(date);
+        console.log(url);
+
+
+        
+}
+
+$
        //$(".shows").append (response.docs[0].headline.main ); 
         
 
